9fc4bd4e2af076e16bd7e2934b938258308611e3
zengxy <xianyu.z@foxmail.com>
2015-04-17 16:52:31 +0800

    0007

---
+++ 0007/countlines.py
@@ -0,0 +1,23 @@
+# 有个目录，里面是你自己写过的程序，统计一下你写过多少行代码。包括空行和注释，但是要分别列出来
+__author__ = 'zengxianyu'
+
+
+def countLines(filepath):
+    total_lines = 0
+    empty_lines = 0
+    comment_lines = 0
+    with open(filepath) as f:
+        for line in f:
+            total_lines += 1
+            if line.strip() == '':
+                empty_lines += 1
+            elif line.strip().startswith("#"):
+                comment_lines += 1
+        f.close()
+        return total_lines, empty_lines, comment_lines
+    return 0, 0, 0
+
+
+if __name__ == '__main__':
+    file = 'countlines.py'
+    print(countLines(file))
